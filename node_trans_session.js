d086d22fd63d7a725ba0c6836c4ae7de78205bbd
datagutt <datagutt@lekanger.no>
2019-12-30 15:50:43 +0100

    Update node_trans_session.js

--- node_trans_session.js
+++ node_trans_session.js
@@ -102,7 +102,7 @@ class NodeTransSession extends EventEmitter {
       const key = `live/archives/${date.getFullYear()}_${(`0${date.getMonth() + 1}`).slice(-2)}/${random}/`;
 
       if (this.conf.rec) {
-        const archive = spawn('node', ['archive.js', random, user, key, ((date - start) / 1000).toFixed(), ouPath]);
+        const archive = spawn('node', ['archive.js', random, this.conf.streamName, key, ((date - start) / 1000).toFixed(), ouPath]);
         archive.stderr.pipe(process.stderr);
         archive.stdout.pipe(process.stdout);
         return;
